5138ae6bf9ac443b9987affa21855e6bcf9c9cc3
aikipezman-pixel <aikipezman@gmail.com>
2026-09-12 17:06:19 -0400

    EmailJSIssue

--- index.js
+++ index.js
@@ -4,21 +4,21 @@
 
 function contact(event) {
     event.preventDefault();
-    const loading = document.querySelector('.modal__contact--loading');
-    const success = document.querySelector('.modal__contact--success');
+    const loading = document.querySelector('.modal__overlay--loading');
+    const success = document.querySelector('.modal__overlay--success');
     loading.classList += " modal__overlay--visible";
 
     emailjs
     .sendForm(
         'service_uuaxmxg',       
         'template_bahjk3k',
-        event.target,
-         'Go7v0kj0Xk2YOR9eH'
+        event.target
     ).then(() => {
         loading.classList.remove("modal__overlay--visible");
         success.classList += " modal__overlay--visible";
         console.log('SUCCESS!');
-  }).catch(() => {
+  }).catch((error) => {
+        console.log('FAILED...', error);
         loading.classList.remove("modal__overlay--visible");
         alert(
             "The Email service is temporarily unavailable. Please contact me directly at aikipezman@gmail.com"
