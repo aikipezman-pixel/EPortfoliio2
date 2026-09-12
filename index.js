2af75aca7fae7f62384190c5b6085c57db9c9ddc
aikipezman-pixel <aikipezman@gmail.com>
2026-09-12 14:06:54 -0400

    JSEmailChapter

--- index.js
+++ index.js
@@ -4,23 +4,29 @@
 
 function contact(event) {
     event.preventDefault();
-//    emailjs
-//    .sendForm(
-//        'service_uuaxmxg',
-//       'template_bahjk3k',
-//        event.target,
-//        'Go7v0kj0Xk2YOR9eH'
-//    ).then(() => {
-//        console.log('SUCCESS!');
-//    })
+    const loading = document.querySelector('.modal__contact--loading');
+    const success = document.querySelector('.modal__contact--success');
+    loading.classList += " modal__overlay--visible";
 
-const loading = document.querySelector('.modal__contact--loading');
-const success = document.querySelector('.modal__contact--success');
-loading.classList += " modal__overlay--visible";
+    emailjs
+    .sendForm(
+        'service_uuaxmxg',       
+        'template_bahjk3k',
+        event.target,
+         'Go7v0kj0Xk2YOR9eH'
+    ).then(() => {
+        loading.classList.remove("modal__overlay--visible");
+        success.classList += " modal__overlay--visible";
+        console.log('SUCCESS!');
+  }).catch(() => {
+        loading.classList.remove("modal__overlay--visible");
+        alert(
+            "The Email service is temporarily unavailable. Please contact me directly at aikipezman@gmail.com"
+        )
+    });
 
 setTimeout(() => {
-    console.log('SUCCESS1!');
+    ;
+console.log('It Worked 1');
 }, 1000);
-loading.classList.remove("modal__overlay--visible");
-success.classList += " modal__overlay--visible";
 }
